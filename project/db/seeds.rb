38f8e31b3663727f718b4f2d3037ba42efec062f
anhtai23 <anhtai23@gmail.com>
2019-06-26 09:09:08 +0700

    thanh cong

--- project/db/seeds.rb
+++ project/db/seeds.rb
@@ -9,13 +9,16 @@ User.create(username: 'user1', password: '123456' ,email: 'user1@gmail.com',role
 User.create(username: 'user2', password: '123456' ,email: 'user2@gmail.com',role: 'r2') 
 User.create(username: 'user3', password: '123456' ,email: 'user3@gmail.com',role: 'r3') 
 User.create(username: 'user4', password: '123456' ,email: 'user4@gmail.com',role: 'r4') 
+User.create(username: 'user4', password: '123456' ,email: 'user4@gmail.com',role: 'r4') 
 
 Photo.create(title: 'ttl1', direction: 'drt1', url: 'img1')
 Photo.create(title: 'ttl2', direction: 'drt2', url: 'img2')
 Photo.create(title: 'ttl3', direction: 'drt3', url: 'img3')
 Photo.create(title: 'ttl4', direction: 'drt4', url: 'img4')
+Photo.create(title: 'ttl4', direction: 'drt4', url: 'img4')
 
 Album.create(title: 'ttl5', direction: 'drt5', url: 'img5')
 Album.create(title: 'ttl6', direction: 'drt6', url: 'img6')
 Album.create(title: 'ttl7', direction: 'drt7', url: 'img7')
+Photo.create(title: 'ttl4', direction: 'drt4', url: 'img4')
 Album.create(title: 'ttl8', direction: 'drt8', url: 'img8')
